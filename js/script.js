0e95b5e743d59e1c63d40c4cd80b9bd059bab6a3
Marcin Świrek <swirek.marcin@gmail.com>
2018-10-29 22:25:42 +0100

    Remove change bcg color functionality. Change hamburger color.

--- js/script.js
+++ js/script.js
@@ -1,33 +1,34 @@
 const navMenu = document.getElementById('nav-bar');
 const headerBtn = document.getElementById('headerBtn');
-const bodyOne = document.getElementById('bodyOne');
+const logo = document.getElementById('navLogo');
+//const bodyOne = document.getElementById('bodyOne');
 
 //Background change
-const gradientOneBtn = document.getElementById('gradientOne');
-const gradientTwoBtn = document.getElementById('gradientTwo');
-const gradientThreeBtn = document.getElementById('gradientThree');
-const gradientFourBtn = document.getElementById('gradientFour');
+// const gradientOneBtn = document.getElementById('gradientOne');
+// const gradientTwoBtn = document.getElementById('gradientTwo');
+// const gradientThreeBtn = document.getElementById('gradientThree');
+// const gradientFourBtn = document.getElementById('gradientFour');
 
-function gradientOneSwitch() {
-    document.body.className = 'body-one';
-}
+// function gradientOneSwitch() {
+//     document.body.className = 'body-one';
+// }
 
-function gradientTwoSwitch() {
-    document.body.className = 'body-two';
-}
+// function gradientTwoSwitch() {
+//     document.body.className = 'body-two';
+// }
 
-function gradientThreeSwitch() {
-    document.body.className = 'body-three';
-}
+// function gradientThreeSwitch() {
+//     document.body.className = 'body-three';
+// }
 
-function gradientFourSwitch() {
-    document.body.className = 'body-four';
-}
+// function gradientFourSwitch() {
+//     document.body.className = 'body-four';
+// }
 
-gradientOneBtn.addEventListener('click', gradientFourSwitch);
-gradientTwoBtn.addEventListener('click', gradientTwoSwitch);
-gradientThreeBtn.addEventListener('click', gradientThreeSwitch);
-gradientFourBtn.addEventListener('click', gradientOneSwitch);
+// gradientOneBtn.addEventListener('click', gradientFourSwitch);
+// gradientTwoBtn.addEventListener('click', gradientTwoSwitch);
+// gradientThreeBtn.addEventListener('click', gradientThreeSwitch);
+// gradientFourBtn.addEventListener('click', gradientOneSwitch);
 
 //Mobile tap event to do
 //gradientOneBtn.addEventListener('touch', gradientFourSwitch);
@@ -42,8 +43,10 @@ function menuScroll() {
         document.documentElement.scrollTop > 100
     ) {
         navMenu.className = 'on-scroll-color';
+        logo.className = 'logo-on-scroll';
     } else {
         navMenu.className = 'navigation';
+        logo.className = 'logo';
     }
 }
 

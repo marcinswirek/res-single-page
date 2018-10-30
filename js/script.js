331389c01347aa9737d64a2e5007688247a01c71
Marcin Świrek <swirek.marcin@gmail.com>
2018-10-30 16:10:29 +0100

    Change portfolio section layout. Give some mar/padd on some elements

--- js/script.js
+++ js/script.js
@@ -1,40 +1,6 @@
 const navMenu = document.getElementById('nav-bar');
 const headerBtn = document.getElementById('headerBtn');
 const logo = document.getElementById('navLogo');
-//const bodyOne = document.getElementById('bodyOne');
-
-//Background change
-// const gradientOneBtn = document.getElementById('gradientOne');
-// const gradientTwoBtn = document.getElementById('gradientTwo');
-// const gradientThreeBtn = document.getElementById('gradientThree');
-// const gradientFourBtn = document.getElementById('gradientFour');
-
-// function gradientOneSwitch() {
-//     document.body.className = 'body-one';
-// }
-
-// function gradientTwoSwitch() {
-//     document.body.className = 'body-two';
-// }
-
-// function gradientThreeSwitch() {
-//     document.body.className = 'body-three';
-// }
-
-// function gradientFourSwitch() {
-//     document.body.className = 'body-four';
-// }
-
-// gradientOneBtn.addEventListener('click', gradientFourSwitch);
-// gradientTwoBtn.addEventListener('click', gradientTwoSwitch);
-// gradientThreeBtn.addEventListener('click', gradientThreeSwitch);
-// gradientFourBtn.addEventListener('click', gradientOneSwitch);
-
-//Mobile tap event to do
-//gradientOneBtn.addEventListener('touch', gradientFourSwitch);
-//gradientTwoBtn.addEventListener('touch', gradientTwoSwitch);
-//gradientThreeBtn.addEventListener('touch', gradientThreeSwitch);
-//gradientFourBtn.addEventListener('touch', gradientOneSwitch);
 
 //Navigation scroll visualisation
 function menuScroll() {

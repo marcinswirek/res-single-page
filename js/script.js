ba7cdeff8ad55b5c16ffecf6939bbbea60697ba1
Marcin Świrek <swirek.marcin@gmail.com>
2018-11-05 18:03:41 +0100

    Add contact form

--- js/script.js
+++ js/script.js
@@ -28,3 +28,27 @@ function showBtn() {
 }
 
 document.addEventListener('DOMContentLoaded', showBtn);
+
+//Contact form
+$(document).ready(function() {
+  return $('#contact-form').submit(function(e) {
+    var email, message, name;
+    name = document.getElementById('inputName');
+    email = document.getElementById('inputEmail');
+    message = document.getElementById('inputMessage');
+    if (!name.value || !email.value || !message.value) {
+      alertify.error('Please check your entries');
+      return false;
+    } else {
+      $.ajax({
+        method: 'POST',
+        url: '//formspree.io/swirek.marcin@gmail.com',
+        data: $('#contact-form').serialize(),
+        datatype: 'json'
+      });
+      e.preventDefault();
+      $(this).get(0).reset();
+      return alertify.success('Message sent');
+    }
+  });
+});
